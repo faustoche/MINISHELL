0f1686f6a37d15079bba81dcd391324766c590b1
Ghislaine Ieong <ghieong@bess-f3r5s7.clusters.42paris.fr>
2025-03-12 17:13:33 +0100

    je retente

--- src/exec/pipe.c
+++ src/exec/pipe.c
@@ -15,45 +15,45 @@
 // DEFINIR NB CMDS ET VOIR POUR LE CMD
 // VOIR L'APPEL A LA FONCTION 
 
-// int	handle_pipe(t_cmd *cmd, int	i, int old_fd)
-// {
-// 	int	pid;
-// 	int	pipefd[2];
+int	handle_pipe(t_cmd *cmd, int	i, int old_fd)
+{
+	int	pid;
+	int	pipefd[2];
 
-// 	if (i >= nb_cmds)
-// 		return (0);
-// 	if (i != nb_cmds - 1)
-// 		pipe(pipefd);
-// 	else
-// 	{
-// 		pipefd[0] = -1;
-// 		pipefd[1] = -1;
-// 	}
-// 	pid = fork();
-// 	if (pid == 0)
-// 	{
-// 		if (old_fd != -1)
-// 		{
-// 			dup2(old_fd, STDIN_FILENO);
-// 			close (old_fd);
-// 		}
-// 		if (i != nb_cmds - 1)
-// 		{
-// 			close(pipefd[0]);
-// 			dup2(pipefd[1], STDOUT_FILENO);
-// 		}
-// 		close(pipefd[1]);
-// 		// execute command
-// 		return (0);
-// 	}
-// 	else
-// 	{
-// 		close(pipefd[1]);
-// 		if (old_fd != -1)
-// 			close(old_fd);
-// 		handle_pipe(cmd, i + 1, pipefd[0]);
-// 	}
-// 	if (i == 0)
-// 		waitpid(pid, NULL, 0);
-// 	return (-1);
-// }
+	if (i >= nb_cmds)
+		return (0);
+	if (i != nb_cmds - 1)
+		pipe(pipefd);
+	else
+	{
+		pipefd[0] = -1;
+		pipefd[1] = -1;
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		if (old_fd != -1)
+		{
+			dup2(old_fd, STDIN_FILENO);
+			close (old_fd);
+		}
+		if (i != nb_cmds - 1)
+		{
+			close(pipefd[0]);
+			dup2(pipefd[1], STDOUT_FILENO);
+		}
+		close(pipefd[1]);
+		// execute command
+		return (0);
+	}
+	else
+	{
+		close(pipefd[1]);
+		if (old_fd != -1)
+			close(old_fd);
+		handle_pipe(cmd, i + 1, pipefd[0]);
+	}
+	if (i == 0)
+		waitpid(pid, NULL, 0);
+	return (-1);
+}
