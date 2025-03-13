07f495d91d6fc136eca4919f9afd179476f2c7e0
ghieong <pan.ghislaine@gmail.com>
2025-03-13 01:04:08 +0100

    tests sur find_pathname

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -12,13 +12,47 @@
 
 #include "minishell.h"
 
-// recuperer PATH (get_env_value)
+// recuperer PATH (getenv)
 // split la chaine
 // while tab non vide, parcourir le tableau et recup la chaine puis concatener avec arg
 // checker si executable trouve (if access == 0)
 // si trouve, sort de la boucle et continue avec execve
 // si trouve nulle part, print ERR_CMD
 
+// char	*find_pathname(char *arg)
+// {
+// 	char	*path_str;
+// 	char	**split_path;
+// 	char	*pathname;
+// 	char	*truc;
+// 	int		i;
+// 	size_t	len_one;
+// 	size_t	len_two;
+
+// 	i = 0;
+// 	truc = NULL;
+// 	path_str = getenv("PATH");
+// 	split_path = ft_split(path_str, ':');
+// 	if (!split_path)
+// 		return (NULL);
+// 	while (split_path && split_path[i])
+// 	{
+// 		len_two = ft_strlen(arg) + ft_strlen(split_path[i]) + 3;
+// 		pathname = malloc(sizeof(char) * len_two);
+// 		if (!pathname)
+// 			return (NULL);
+// 		ft_strcpy(pathname, split_path[i]);
+// 		ft_strcpy(truc, "/");
+// 		len_one = ft_strlen(arg) + ft_strlen(truc) + 1;
+// 		ft_strlcat(arg, truc, len_one);
+// 		ft_strlcat(pathname, arg, len_two);
+// 		if (!access(pathname, F_OK))
+// 			break ;
+// 		i++;
+// 	}
+// 	free_tokens(split_path);
+// 	return (pathname);
+// }
 
 char	*find_pathname(char *arg)
 {
@@ -34,17 +68,15 @@ char	*find_pathname(char *arg)
 	return (pathname);
 }
 
-
-
 void	execute_commands(t_cmd *cmd)
 {
 	t_cmd	*current;
 	int		pid;
 	char	*pathname;
 
-
 	current = cmd;
 	pathname = find_pathname(current->args[0]);
+	printf("%s\n", pathname);
 	while (current)
 	{
 		if (current->args && current->args[0] && is_builtins(current->args[0]))
@@ -59,7 +91,7 @@ void	execute_commands(t_cmd *cmd)
 			}
          	if (pid == 0) //dans process fils
 			{
-				if (access(pathname, F_OK) == -1 || access(pathname, X_OK) == -1)
+				if (access(pathname, X_OK) == -1)
 					printf(ERR_CMD, current->args[0]);
 				else if (execve(pathname, current->args, NULL) == -1)
 				{
@@ -75,7 +107,6 @@ void	execute_commands(t_cmd *cmd)
 	free(pathname);
 	free(current);
 }
-
 
 // void	execute_commands(t_cmd *cmd)
 // {
