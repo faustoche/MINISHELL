eb7a9653f2d263d768b01fbb16ce7c02b33fe3f8
Faustine Crocq <fcrocq@bess-f3r6s4.clusters.42paris.fr>
2025-04-08 11:21:45 +0200

    unset double arg

--- src/exec/exec_builtins.c
+++ src/exec/exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: fcrocq <fcrocq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 14:58:50 by fcrocq            #+#    #+#             */
-/*   Updated: 2025/04/07 09:20:07 by fcrocq           ###   ########.fr       */
+/*   Updated: 2025/04/08 11:20:28 by fcrocq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ int	is_builtins(char *cmd)
 
 void	builtins_execution(t_cmd *cmd, t_env **env_list)
 {
+	size_t	i;
+
 	if (!cmd || !cmd->args || !cmd->args[0])
 		return ;
 	if (ft_strcmp(cmd->args[0], "echo") == 0)
@@ -43,13 +45,30 @@ void	builtins_execution(t_cmd *cmd, t_env **env_list)
 		*env_list = ft_cd(cmd, *env_list);
 	if (ft_strcmp(cmd->args[0], "export") == 0)
 	{
-		if (cmd->args[1])
-			*env_list = ft_export(*env_list, cmd->args[1]);
-		else
+		if (cmd->nb_arg == 1)
 			*env_list = ft_export(*env_list, NULL);
+		else
+		{
+			i = 1;
+			while (i < cmd->nb_arg)
+			{
+				*env_list = ft_export(*env_list, cmd->args[i]);
+				i++;
+			}
+		}
 	}
 	if (ft_strcmp(cmd->args[0], "unset") == 0 && cmd->args[1])
-		*env_list = ft_unset(*env_list, cmd->args[1]);
+	{
+		if (cmd->nb_arg > 1)
+		{
+			i = 1;
+			while (i < cmd->nb_arg)
+			{
+				*env_list = ft_unset(*env_list, cmd->args[i]);
+				i++;
+			}
+		}
+	}
 	if (ft_strcmp(cmd->args[0], "env") == 0)
 		ft_env(*env_list);
 	if (ft_strcmp(cmd->args[0], "exit") == 0)
