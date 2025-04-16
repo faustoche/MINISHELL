12c279a345248cf42e8daaea8b56991e65e8d8a5
faustoche <faustine.crocq@gmail.com>
2025-04-16 16:33:14 +0200

    aaaaaaah

--- src/parsing/parse_heredoc.c
+++ src/parsing/parse_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: fcrocq <fcrocq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 09:51:22 by fcrocq            #+#    #+#             */
-/*   Updated: 2025/04/16 16:15:49 by fcrocq           ###   ########.fr       */
+/*   Updated: 2025/04/16 16:31:36 by fcrocq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,6 +115,6 @@ int	handle_all_heredocs(t_cmd *cmd)
 	}
 	handle_signals(SIGINT, PROMPT);
 	handle_signals(SIGQUIT, IGNORE);
-	close_all_fd(3);
+	//close_all_fd(3);
 	return (0);
 }
